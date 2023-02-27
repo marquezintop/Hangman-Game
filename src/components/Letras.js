c7891447c52b02a9d1cbfea2def10d23d331edeb
marquez <marquez.cmbh@gmail.com>
2023-02-27 17:37:05 -0300

    commit

---
+++ src/components/Letras.js
@@ -0,0 +1,13 @@
+function Letras({disabled, letter, searchLetter}){
+    return(
+        <>
+            <button 
+            data-test="letter"
+            disabled={disabled}
+            onClick={searchLetter}> 
+            {letter} 
+            </button>
+        </>
+    )
+}
+export default Letras
